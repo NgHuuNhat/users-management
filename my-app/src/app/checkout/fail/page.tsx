a9b42ca2d23d1bc313a874b9f799244fa1762124
NgHuuNhat <nhat200901@gmail.com>
2026-06-20 16:06:55 +0700

    updatetetete

--- my-app/src/app/checkout/fail/page.tsx
+++ my-app/src/app/checkout/fail/page.tsx
@@ -2,9 +2,13 @@ import { Suspense } from 'react';
 import FailComponent from './FailComponent';
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div>Đang tải...</div>}>
-      <FailComponent />
-    </Suspense>
-  );
+    return (
+        <Suspense fallback={
+            <div className="flex items-center justify-center h-screen">
+                Loading...
+            </div>
+        }>
+            <FailComponent />
+        </Suspense>
+    );
 }
