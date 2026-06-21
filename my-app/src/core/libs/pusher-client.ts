747ca4c360ef7a6a464a3424aaa64ab294fd09f9
NgHuuNhat <nhat200901@gmail.com>
2026-06-21 07:54:37 +0700

    update-full-pro3

--- my-app/src/core/libs/pusher-client.ts
+++ my-app/src/core/libs/pusher-client.ts
@@ -13,7 +13,7 @@ let pusher: any;
 
 export function getPusher() {
   if (!pusher && typeof window !== 'undefined') {
-    pusher = new Pusher(process.env.NEXT_PUBLIC_PUSHER_KEY!, {
+    pusher = new Pusher('85a671177c6acaa8de58', {
       cluster: 'ap1',
     });
   }
