39e70daf0437b37a37b4e6c566e6ad8e38276d90
NgHuuNhat <nhat200901@gmail.com>
2026-06-26 08:15:11 +0700

    update-package-jose

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  serverExternalPackages: ["firebase-admin"],
+  // serverExternalPackages: ["firebase-admin"],
+  serverExternalPackages: ['firebase-admin', 'jwks-rsa', 'jose'],
 };
 
 export default nextConfig;
